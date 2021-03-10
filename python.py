3c07a8ff6677d537a43978e2c20cd6da0222915e
ihzaaji <80429087+ihzaaji@users.noreply.github.com>
2021-03-11 05:50:54 +0700

    membuat file python.py

---
+++ python.py
@@ -0,0 +1,10 @@
+print ("perulangan for and while")
+
+print("untuk for")
+
+nama_malaikat=("izrail,munkar,nakir,malik")
+for i in nama_malaikat:print(i)
+  
+print("untuk while")
+
+i= 1 while i<=5:print("ihza dirgantara aji")i+=3
