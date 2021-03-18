7b2c7005ae5dc155068e7798fd16321d12682117
ihzaaji <80429087+ihzaaji@users.noreply.github.com>
2021-03-18 12:56:19 +0800

    Add files via upload

---
+++ TUGAS.python.py
@@ -0,0 +1,16 @@
+def intro():
+    print("\n NAMA:IHZA DIRGANTARA AJI \n NIM: TI17200015 \n prodi: TEKNIK INFORMATIKA (A)")
+    print("\n mr.bekicot memiliki sebuah gedung dengan tinggi 100m,\n pada siang hari dia naik setinggi 5m, \n dan pada malam hari dia merosot 4m \n jadi, untuk bisa sampai")
+
+intro()
+
+print("\n jawab")
+
+def gedung():
+    print("\n jika mr.bekicot siang hari naik setinggi 5m dan pada malam hari turun 4m dan ketinggian gedung 100m, \n maka diperoleh ((naik-turun) * tinggi)=(5-4) * 100 \n =1")
+    print("\n jadi, untuk mencapai pucak gedung mr.bekicot membutuhkan waktu",(5-4)*100),print("hari")
+
+gedung()
+
+#tugas II
+
